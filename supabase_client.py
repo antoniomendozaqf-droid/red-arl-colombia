ba5719f275cd984fc1b789a00ca06659c7e1acb9
antoniomendozaqf-droid <antoniomendozaqf@gmail.com>
2026-08-27 09:30:22 -0500

    Create supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,13 @@
+import os
+from supabase import create_client, Client
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Faltan SUPABASE_URL o SUPABASE_KEY")
+
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
